91d95fe73845226e81f1fbec793689591c81479c
Sherman Rose <sherman@shermanrose.uk>
2020-12-14 22:35:26 +0000

    Removed build files and added base scenes

---
+++ numbers/src/Scenes/NumberGameScene.ts
@@ -0,0 +1,32 @@
+export default class NumbersGame extends Phaser.Scene
+{
+    constructor ()
+    {
+        super('NumbersGame');
+    }
+
+    preload ()
+    {
+        // this.load.image('logo', 'assets/phaser3-logo.png');
+    }
+
+    create ()
+    {
+        // this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+
+
+        // this.add.image(400, 300, 'libs');
+
+        // const logo = this.add.image(400, 70, 'logo');
+
+        // this.tweens.add({
+        //     targets: logo,
+        //     y: 350,
+        //     duration: 1500,
+        //     ease: 'Sine.inOut',
+        //     yoyo: true,
+        //     repeat: -1
+        // })
+    }
+}
+
--- numbers/src/game.ts
+++ numbers/src/game.ts
@@ -1,44 +1,21 @@
 import 'phaser';
+import WebFontLoaderPlugin from 'phaser3-rex-plugins/plugins/webfontloader-plugin.js';
+import NumberGameScene from './Scenes/NumberGameScene'
 
-export default class NumbersGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('demo');
-    }
-
-    preload ()
-    {
-        // this.load.image('logo', 'assets/phaser3-logo.png');
-    }
-
-    create ()
-    {
-        // this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-
-        // this.add.image(400, 300, 'libs');
-
-        // const logo = this.add.image(400, 70, 'logo');
-
-        // this.tweens.add({
-        //     targets: logo,
-        //     y: 350,
-        //     duration: 1500,
-        //     ease: 'Sine.inOut',
-        //     yoyo: true,
-        //     repeat: -1
-        // })
-    }
-}
 
 const config = {
     type: Phaser.AUTO,
-    backgroundColor: '#125555',
+    backgroundColor: '#D3D3D3',
     width: 720,
     height: 1280,
-    scene: NumbersGame,
-    canvas: document.querySelector('canvas');
+    scene: NumberGameScene,
+    plugins: {
+        global: [{
+            key: 'rexWebFontLoader',
+            plugin: WebFontLoaderPlugin,
+            start: true
+        }]
+    }
 };
 
 const game = new Phaser.Game(config);
