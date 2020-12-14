9661a1f5f77740deaf8790943057859a07fffffb
Sherman Rose <sherman@shermanrose.uk>
2020-12-14 17:50:43 +0000

    Numbers. GAME START

--- numbers/src/game.ts
+++ numbers/src/game.ts
@@ -1,6 +1,6 @@
 import 'phaser';
 
-export default class Demo extends Phaser.Scene
+export default class NumbersGame extends Phaser.Scene
 {
     constructor ()
     {
@@ -9,39 +9,36 @@ export default class Demo extends Phaser.Scene
 
     preload ()
     {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+        // this.load.image('logo', 'assets/phaser3-logo.png');
     }
 
     create ()
     {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+        // this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
 
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
 
-        this.add.image(400, 300, 'libs');
+        // this.add.image(400, 300, 'libs');
 
-        const logo = this.add.image(400, 70, 'logo');
+        // const logo = this.add.image(400, 70, 'logo');
 
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
+        // this.tweens.add({
+        //     targets: logo,
+        //     y: 350,
+        //     duration: 1500,
+        //     ease: 'Sine.inOut',
+        //     yoyo: true,
+        //     repeat: -1
+        // })
     }
 }
 
 const config = {
     type: Phaser.AUTO,
     backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
+    width: 720,
+    height: 1280,
+    scene: NumbersGame,
+    canvas: document.querySelector('canvas');
 };
 
 const game = new Phaser.Game(config);
