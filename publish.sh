ae828ddf7d920f429b4d462afd7aaad8095f90b7
Sherman Rose <sherman@shermanrose.uk>
2021-01-14 03:58:58 +0000

    Add build to simple publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 TIMESTAMP="$(date +%s)"
+
+npm run dev
+[[ "$?" -gt 0 ]] && { echo "Failed to build dev package"; exit 1; }
+npm run build
+[[ "$?" -gt 0 ]] && { echo "Failed to build package"; exit 1; }
  
 sed -e "s/TIMESTAMP/$TIMESTAMP/" -e "s/PACKAGE/game/" index-template.html > index.html
 sed -e "s/TIMESTAMP/$TIMESTAMP/" -e "s/PACKAGE/game-dev/" index-template.html > index-dev.html
